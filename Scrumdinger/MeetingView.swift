2d2f5488463d2a2a91bf2f03453f595229a94844
reinexxism <lucy0805@naver.com>
2024-06-24 19:36:28 +0900

    Supplement accessibility data: VoiceOver 관련 UX를 수정하였습니다.

--- Scrumdinger/MeetingView.swift
+++ Scrumdinger/MeetingView.swift
@@ -24,6 +24,9 @@ struct MeetingView: View {
                     Label("600", systemImage: "hourglass.bottomhalf.fill")
                 }
             }
+            .accessibilityElement(children: /*@START_MENU_TOKEN@*/.ignore/*@END_MENU_TOKEN@*/)
+            .accessibilityLabel("Time Remaining")
+            .accessibilityValue("10 minutes")
             Circle()
                 .strokeBorder(lineWidth: 20)
             HStack {
@@ -32,6 +35,7 @@ struct MeetingView: View {
                 Button(action: {}) {
                     Image(systemName: "forward.fill")
                 }
+                .accessibilityLabel("Next Speaker")
             }
         }
         .padding()

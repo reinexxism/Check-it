ce3c59996c8e5b1252f987dbb7549260cff2db65
reinexxism <lucy0805@naver.com>
2024-06-24 19:01:41 +0900

    compose views

---
+++ Scrumdinger/MeetingView.swift
@@ -0,0 +1,30 @@
+//
+//  ContentView.swift
+//  Scrumdinger
+//
+//  Created by 최주현 on 6/24/24.
+//
+
+import SwiftUI
+
+struct MeetingView: View {
+    var body: some View {
+        VStack {
+            ProgressView(value: 5, total: 15)
+            HStack {
+                VStack {
+                    Text("Seconds Elapsed")
+                    Label("300", systemImage: "hourglass.tophalf.fill")
+                }
+                VStack {
+                    Text("Seconds Remaining")
+                    Label("600", systemImage: "hourglass.bottomhalf.fill")
+                }
+            }
+        }
+    }
+}
+
+#Preview {
+    MeetingView()
+}

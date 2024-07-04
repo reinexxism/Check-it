670cd7e66c21347a2287a3055d695d19eb283c2e
reinexxism <lucy0805@naver.com>
2024-07-04 21:20:03 +0900

    Display a list of daily scrums: List View 활용

---
+++ Scrumdinger/ScrumsView.swift
@@ -0,0 +1,23 @@
+//
+//  ScrumsView.swift
+//  Scrumdinger
+//
+//  Created by 최주현 on 7/4/24.
+//
+
+import SwiftUI
+
+struct ScrumsView: View {
+    let scrums: [DailyScrum]
+    
+    var body: some View {
+        List(scrums, id: \.title) { scrum in
+            CardView(scrum: scrum)
+                .listRowBackground(scrum.theme.mainColor)
+        }
+    }
+}
+
+#Preview {
+    ScrumsView(scrums: DailyScrum.sampleData)
+}

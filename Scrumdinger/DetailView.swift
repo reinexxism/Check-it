868badcbdd62bedc09e6190a320e262aaae6d255
reinexxism <lucy0805@naver.com>
2024-07-04 21:48:42 +0900

    DetailView 생성 및 이동 구현

---
+++ Scrumdinger/DetailView.swift
@@ -0,0 +1,22 @@
+//
+//  DetailView.swift
+//  Scrumdinger
+//
+//  Created by 최주현 on 7/4/24.
+//
+
+import SwiftUI
+
+struct DetailView: View {
+    let scrum: DailyScrum
+    
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    NavigationStack {
+        DetailView(scrum: DailyScrum.sampleData[0])
+    }
+}
